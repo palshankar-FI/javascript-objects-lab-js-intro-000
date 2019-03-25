479f8c91ec4468d8b2c7d6932a0fe516d627c790
PS <palshankar@outlook.com>
2019-03-25 08:37:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,17 +21,3 @@ function destructivelyDeleteFromObjectByKey(obj, key) {
   delete obj[key];
   return(obj);
 }
-  it('returns object without the delete key/value pair', function() {
-    var obj = { prop: 1 }
-    var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
-
-    expect(newObj['prop']).toBe(undefined)
-  })
-
-  it('modifies the original object', function() {
-    var obj = { prop: 1 }
-    var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
-
-    expect(obj['prop']).toBe(undefined)
-  })
-})
