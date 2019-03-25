e28d95272ee3538a13f3a1b7c251864d1d2a7e4f
PS <palshankar@outlook.com>
2019-03-25 08:29:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,33 +1,7 @@
 var recipes = {};
 
 
-
 function updateObjectWithKeyAndValue(obj, key, value) {
   var newObj = Object.assign({}, obj, { [key] : value});
   return(newObj);
 }
-  it('returns an object with the orignal key value pairs and the new key value pair', function() {
-    var obj = { prop: 1 }
-
-    expect(updateObjectWithKeyAndValue(obj, 'prop2', 2)).toMatch({
-      prop: 1,
-      prop2: 2
-    })
-  })
-
-  it('does not modify the original object, but rather returns a clone with the new data', function() {
-    var obj = { prop: 1 }
-
-    updateObjectWithKeyAndValue(obj, 'prop2', 2)
-
-    expect(obj['prop2']).toBe(undefined)
-  })
-
-  it('returns an object with an updated key value pair', function() {
-    var obj = { prop: 'old value' }
-
-    var newObj = updateObjectWithKeyAndValue(obj, 'prop', 'new value')
-
-    expect(newObj['prop']).toBe('new value')
-  })
-})
