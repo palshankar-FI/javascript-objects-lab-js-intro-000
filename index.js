7622ebe744b11bb62ac80957a04d40dca544cf0a
PS <palshankar@outlook.com>
2019-03-25 08:34:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,23 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
   obj[key] = value;
   return(obj);
 }
+
+function deleteFromObjectByKey(obj, key) {
+  var newObj = Object.assign({}, obj);
+  delete(newObj[key]);
+  return(newObj);
+}
+  it('deletes `key` from a clone of object and returns the new object (it is non-destructive)', function() {
+    var obj = { prop: 1 }
+    var newObj = deleteFromObjectByKey(obj, 'prop')
+
+    expect(newObj['prop']).toBe(undefined)
+  })
+
+  it('does not modify the original object (it is non-destructive)', function() {
+    var obj = { prop: 1 }
+
+    deleteFromObjectByKey(obj, 'prop')
+    expect(obj['prop']).toBe(1)
+  })
+})
