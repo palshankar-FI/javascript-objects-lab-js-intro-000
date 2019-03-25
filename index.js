3862eec5423ca5bece01972d8f6be17676ee2569
PS <palshankar@outlook.com>
2019-03-25 08:30:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,22 @@ function updateObjectWithKeyAndValue(obj, key, value) {
   var newObj = Object.assign({}, obj, { [key] : value});
   return(newObj);
 }
+
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  
+}
+  it('updates `object` with the given `key` and `value` (it is destructive) and returns the entire updated object', function() {
+    var obj = { prop: 1 }
+
+    expect(destructivelyUpdateObjectWithKeyAndValue(obj, 'prop2', 2)).toMatch({
+      prop: 1,
+      prop2: 2
+    })
+
+    expect(obj).toMatch({
+      prop: 1,
+      prop2: 2
+    })
+  })
+})
